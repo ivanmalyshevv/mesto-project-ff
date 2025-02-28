256419fb747d67e5a9f0b5104fef52ef6b0815b3
Ivan Malyshev <ivan.malyshevv@gmail.com>
2025-02-28 15:21:06 +0300

    fix3

--- src/index.js
+++ src/index.js
@@ -4,35 +4,38 @@ import { openModal, closeModal, addPopupListeners } from './components/modal.js'
 import { enableValidation, clearValidation } from './components/validation.js';
 import { getUserInfo, getInitialCards, updateProfileInfo, addNewCard, deleteCard, likeCard, unlikeCard, updateAvatar } from './components/api.js';
 
-// DOM узлы
+// DOM узлы 
 const placesList = document.querySelector('.places__list');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__description');
 const profileAvatar = document.querySelector('.profile__avatar'); 
 
-// Получаем попапы
+// Получаем попапы по их классам 
 const editProfilePopup = document.querySelector('.popup_type_edit');
 const addCardPopup = document.querySelector('.popup_type_new-card');
 const imagePopup = document.querySelector('.popup_type_image');
 const avatarPopup = document.querySelector('.popup_type_avatar');
 
-// Кнопки
+// Получаем кнопки редактирования и добавления карточек 
 const editProfileButton = document.querySelector('.profile__edit-button');
 const addCardButton = document.querySelector('.profile__add-button');
 const editAvatarButton = document.querySelector('.profile__image');
 
-// Формы
+// Получаем форму и поля ввода из попапа редактирования профиля 
 const profileFormElement = editProfilePopup.querySelector('.popup__form');
 const nameInput = editProfilePopup.querySelector('.popup__input_type_name');
 const jobInput = editProfilePopup.querySelector('.popup__input_type_description');
 
+// Получаем форму и поля ввода из попапа добавления карточки 
 const addCardFormElement = addCardPopup.querySelector('.popup__form');
 const cardNameInput = addCardPopup.querySelector('.popup__input_type_card-name');
 const cardLinkInput = addCardPopup.querySelector('.popup__input_type_url');
 
+// Получаем форму и поля ввода из попапа обновления аватара 
 const avatarFormElement = avatarPopup.querySelector('.popup__form');
 const avatarInput = avatarFormElement.querySelector('.popup__input_type_url');
 
+// Получаем элементы попапа с изображением 
 const imageModal = imagePopup.querySelector('.popup__image');
 const imageCaption = imagePopup.querySelector('.popup__caption');
 
@@ -47,16 +50,19 @@ const validationConfig = {
 
 enableValidation(validationConfig);
 
+// Функция для отображения карточки 
 function renderCard(cardElement) {
   placesList.prepend(cardElement);
 }
 
+// Функция для удаления карточки 
 function handleDeleteCard(cardId, cardElement) {
     deleteCard(cardId)
         .then(() => cardElement.remove())
         .catch(err => console.error('Ошибка при удалении карточки:', err));
 }
 
+// Функция поставить/снять лайк 
 function cardLike(cardId, likeButton, likesCount) {
     const isLiked = likeButton.classList.contains('card__like-button_is-active');
     (isLiked ? unlikeCard(cardId) : likeCard(cardId))
@@ -67,11 +73,13 @@ function cardLike(cardId, likeButton, likesCount) {
         .catch(err => console.error('Ошибка при лайке:', err));
 }
 
+// Функция для заполнения полей формы текущими значениями 
 function fillProfileForm() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
+// Обработчик отправки формы редактирования профиля 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   const submitButton = evt.submitter;
@@ -88,6 +96,7 @@ function handleProfileFormSubmit(evt) {
     .finally(() => submitButton.textContent = originalText);
 }
 
+// Обработчик отправки формы добавления карточки 
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
   const submitButton = evt.submitter;
@@ -105,6 +114,7 @@ function handleAddCardSubmit(evt) {
     .finally(() => submitButton.textContent = originalText);
 }
 
+// Обработчик отправки формы обновления аватара 
 function handleAvatarFormSubmit(evt) {
     evt.preventDefault();
     const submitButton = evt.submitter;
@@ -121,6 +131,7 @@ function handleAvatarFormSubmit(evt) {
       .finally(() => submitButton.textContent = originalText);
 }
 
+// Функция для открытия попапа с изображением
 function openImagePopup(data) {
   imageModal.src = data.link;
   imageModal.alt = data.name;
@@ -128,31 +139,37 @@ function openImagePopup(data) {
   openModal(imagePopup);
 }
 
+// Прикрепляем обработчики к формам 
 profileFormElement.addEventListener('submit', handleProfileFormSubmit);
 addCardFormElement.addEventListener('submit', handleAddCardSubmit);
 avatarFormElement.addEventListener('submit', handleAvatarFormSubmit);
 
+// Слушатели событий для попапов 
 addPopupListeners(editProfilePopup);
 addPopupListeners(addCardPopup);
 addPopupListeners(imagePopup);
 addPopupListeners(avatarPopup);
 
+// Открытие попапа редактирования профиля 
 editProfileButton.addEventListener('click', () => {
   fillProfileForm();
   clearValidation(profileFormElement, validationConfig);
   openModal(editProfilePopup);
 });
 
+// Открытие попапа добавления карточки 
 addCardButton.addEventListener('click', () => {
   clearValidation(addCardFormElement, validationConfig);
   openModal(addCardPopup);
 });
 
+// Открытие попапа обновления аватара 
 editAvatarButton.addEventListener('click', () => {
   clearValidation(avatarFormElement, validationConfig);
   openModal(avatarPopup);
 });
 
+// Получение данных с сервера 
 let userData;
 
 Promise.all([getUserInfo(), getInitialCards()])
